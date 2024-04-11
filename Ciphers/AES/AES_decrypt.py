4a4469ea72244d218ff4d705c6a1fd9ee16e0239
Carson-Spaniel <carsonspaniel@gmail.com>
2024-04-10 23:35:34 -0500

    adding hash password encryption

--- Ciphers/AES/AES_decrypt.py
+++ Ciphers/AES/AES_decrypt.py
@@ -1,6 +1,13 @@
 from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
 import os
 import sys
+import getpass
+
+def hash_string(input_string):
+    sha256 = SHA256.new()
+    sha256.update(input_string.encode('utf-8'))
+    return sha256.digest()
 
 def unpkcs_7(padded_text):
     pad_byte = padded_text[-1]
@@ -13,11 +20,7 @@ def unpkcs_7(padded_text):
 
     return padded_text[:-pad_byte]
 
-def decrypt_file(file_path, key_path):
-    key_file = key_path
-
-    with open(key_file, "rb") as f:
-        aes_key = f.read(32)
+def decrypt_file(file_path, aes_key):
 
     encrypted_file = file_path + "_encrypted.bin"
 
@@ -44,31 +47,39 @@ def decrypt_file(file_path, key_path):
     except ValueError:
         print(f"Failed to decrypt '{file_path}_encrypted.bin'")
         
-def decrypt_folder(folder_path, key_path):
+def decrypt_folder(folder_path, aes_key):
     for root, _, files in os.walk(folder_path):
         for file in files:
             if file.endswith("_encrypted.bin"):
                 file_path = os.path.join(root, file.replace("_encrypted.bin", ""))
-                decrypt_file(file_path, key_path)
+                decrypt_file(file_path, aes_key)
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: py AES_decrypt.py <folder_path> <key_file>")
+    if len(sys.argv) == 2:
+        key_string = getpass.getpass("Enter encryption password: ")
+        aes_key = hash_string(key_string)
+
+    elif len(sys.argv) == 3:
+        key_file = sys.argv[2]
+
+        if not os.path.exists(key_file):
+            print(f"Key file '{key_file}' not found.")
+            sys.exit(1)
+
+        with open(sys.argv[2], 'rb') as f:
+            aes_key = f.read()
+    else:
+        print("Usage: \n\tpy AES_decrypt.py <file_or_folder_path> <key_file> or\n\tpy AES_decrypt.py <file_or_folder_path>")
         sys.exit(1)
 
     path = sys.argv[1]
-    key_file = sys.argv[2]
-
-    if not os.path.exists(key_file):
-        print(f"Key file '{key_file}' not found.")
-        sys.exit(1)
 
     if os.path.isfile(path):
         # Decrypt a single file
-        decrypt_file(os.path.splitext(path)[0].replace("_encrypted", ""), key_file)
+        decrypt_file(os.path.splitext(path)[0].replace("_encrypted", ""), aes_key)
     elif os.path.isdir(path):
         # Decrypt all files in a folder
-        decrypt_folder(path, key_file)
+        decrypt_folder(path, aes_key)
     else:
         print(f"Error: '{path}' is not a valid file or folder.")
         sys.exit(1)
--- Ciphers/AES/AES_encrypt.py
+++ Ciphers/AES/AES_encrypt.py
@@ -1,6 +1,13 @@
 from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
 import os
 import sys
+import getpass
+
+def hash_string(input_string):
+    sha256 = SHA256.new()
+    sha256.update(input_string.encode('utf-8'))
+    return sha256.digest()
 
 def pkcs_7(plaintext, blocksize):
     blocks = [plaintext[i:i+blocksize] for i in range(0, len(plaintext), blocksize)]
@@ -12,9 +19,7 @@ def pkcs_7(plaintext, blocksize):
 
     return b"".join(blocks)
 
-def encrypt_file(file_path, encrypt_dir, key_path):
-    with open(key_path, 'rb') as f:
-        aes_key = f.read()
+def encrypt_file(file_path, encrypt_dir, aes_key):
 
     with open(file_path, 'rb') as f:
         data = f.read()
@@ -43,6 +48,9 @@ def encrypt_file(file_path, encrypt_dir, key_path):
         f.write(cipher.nonce)
         f.write(ciphertext)
 
+    with open(f"key.bin", "wb") as f:
+        f.write(aes_key)
+
     print(f'Encrypted {file_info[0]}{file_info[1]}')
 
 def encrypt_folder(folder_path, encrypt_dir, key_path):
@@ -52,21 +60,34 @@ def encrypt_folder(folder_path, encrypt_dir, key_path):
             encrypt_file(file_path, encrypt_dir, key_path)
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: py AES_encrypt.py <file_or_folder_path> <key_file>")
+    if len(sys.argv) == 2:
+        while True:
+            key_string = getpass.getpass("Enter encryption password: ")
+            confirm_key_string = getpass.getpass("Confirm encryption password: ")
+
+            if key_string == confirm_key_string:
+                aes_key = hash_string(key_string)
+                break
+            else:
+                print("Passwords do not match. Please try again.")
+
+    elif len(sys.argv) == 3:
+        with open(sys.argv[2], 'rb') as f:
+            aes_key = f.read()
+    else:
+        print("Usage: \n\tpy AES_encrypt.py <file_or_folder_path> <key_file> or\n\tpy AES_encrypt.py <file_or_folder_path>")
         sys.exit(1)
 
     path = sys.argv[1]
-    key_path = sys.argv[2]
     encrypt_dir = './Encrypted/'
 
     if os.path.isfile(path):
         # Encrypt a single file
-        encrypt_file(path, encrypt_dir, key_path)
+        encrypt_file(path, encrypt_dir, aes_key)
         print(f"File '{path}' encrypted.")
     elif os.path.isdir(path):
         # Encrypt all files in a folder
-        encrypt_folder(path, encrypt_dir, key_path)
+        encrypt_folder(path, encrypt_dir, aes_key)
         print(f"All files in folder '{path}' encrypted and saved in '{encrypt_dir}'.")
     else:
         print(f"Error: '{path}' is not a valid file or folder.")
